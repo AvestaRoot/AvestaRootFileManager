43f5429f08951a1a25af1756d152846c2c1c0e36
Mahdi Nezam Parast <mahdinezamparast7@gmail.com>
2021-08-20 17:40:11 +0430

    I coded a big part of fragment_main.xml

--- app/src/main/java/ir/avestaroot/my/MainActivity.kt
+++ app/src/main/java/ir/avestaroot/my/MainActivity.kt
@@ -3,6 +3,8 @@ package ir.avestaroot.my
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import ir.avestaroot.my.databinding.ActivityMainBinding
 
@@ -14,7 +16,33 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         init()
-        
+        initAppbar()
+
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId) {
+            android.R.id.home -> {
+
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun initAppbar() {
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.fragment_main_menu, menu)
+
+        return super.onCreateOptionsMenu(menu)
     }
 
     private fun init() {
