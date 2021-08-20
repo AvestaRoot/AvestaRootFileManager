637da0d86fdec77e3f3be2fffdce18cddd16a422
Mahdi Nezam Parast <mahdinezamparast7@gmail.com>
2021-08-20 16:32:16 +0430

    I created the essential class for navigating between fragments. now, coding SplashActivity is done

---
+++ app/src/main/java/ir/avestaroot/my/SplashActivity.kt
@@ -0,0 +1,24 @@
+package ir.avestaroot.my
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+
+    private val interval = 2000L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed(Runnable {
+
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+        }, interval)
+    }
+}
