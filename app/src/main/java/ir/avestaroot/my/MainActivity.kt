553570488b62d5a42fed0e0fd9f203b35942ea91
Mahdi Nezam Parast <mahdinezamparast7@gmail.com>
2021-08-21 18:23:52 +0430

    Prepared the master branch now it has the MainActivity in itself.

---
+++ app/src/main/java/ir/avestaroot/my/MainActivity.kt
@@ -0,0 +1,20 @@
+package ir.avestaroot.my.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ir.avestaroot.my.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        init()
+    }
+
+    private fun init() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
