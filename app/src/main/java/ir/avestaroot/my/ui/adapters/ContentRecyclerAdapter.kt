a2028dc0593b2d3903d7186bcfddbc0be38504e4
Ali <treeroot.ir@gmail.com>
2021-08-27 10:36:18 +0430

    update some code in mainActivity

--- app/src/main/java/ir/avestaroot/my/ui/adapters/ContentRecyclerAdapter.kt
+++ app/src/main/java/ir/avestaroot/my/ui/adapters/ContentRecyclerAdapter.kt
@@ -1,20 +1,14 @@
 package ir.avestaroot.my.ui.adapters
 
-import android.content.Context
+import android.annotation.SuppressLint
 import android.graphics.Color
-import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.RelativeLayout
-import android.widget.Toast
 import androidx.recyclerview.widget.AsyncDifferConfig
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import ir.avestaroot.my.data.model.ContentItem
-import ir.avestaroot.my.data.model.StorageItem
 import ir.avestaroot.my.databinding.ItemStorageBinding
 
 class ContentRecyclerAdapter() :
@@ -58,24 +52,28 @@ class ContentRecyclerAdapter() :
     inner class ViewHolder(private val binding: ItemStorageBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
+        @SuppressLint("ResourceAsColor", "Range")
         fun bind(position: Int) {
             val item = getItem(position)
 
            // Log.d("myapplog", "bind")
 
             binding.nameTv.text = item.title
+            val yellow = Color.parseColor("#63FFAD15")
 
             binding.relativeItems.setOnLongClickListener {
                 binding.relativeItems.setSelected(!item.isSelected)
-                binding.relativeItems.setBackgroundColor(if (item.isSelected) Color.WHITE else Color.CYAN)
+                binding.relativeItems.setBackgroundColor(if (item.isSelected) Color.WHITE else yellow)
 
                 true
             }
 
-            binding.relativeItems.setOnClickListener {
-                binding.relativeItems.setSelected(!item.isSelected)
-                binding.relativeItems.setBackgroundColor(if (item.isSelected) Color.CYAN else Color.WHITE)
-            }
+
+                binding.relativeItems.setOnClickListener {
+                    binding.relativeItems.setSelected(!item.isSelected)
+                    binding.relativeItems.setBackgroundColor(if (item.isSelected ) yellow  else Color.WHITE)
+                }
+
             /*binding.itemsCountTv.text = item.itemsCount
             binding.dateTv.text = item.dateAdded
 
